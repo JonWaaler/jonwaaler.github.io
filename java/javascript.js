5a3c23e97c7a9d96bcbc360cf5d7671b04b047bc
JonWaaler <jonwaaler@outlook.com>
2022-10-11 21:07:54 -0400

    updated games, added project tab selection between webdev/gamedev

--- java/javascript.js
+++ java/javascript.js
@@ -2,4 +2,25 @@ $(document).ready(function () {
   $("#button").click(function () {
     $("#navigation").toggleClass("show");
   });
+
+  function toggletMyWork(showGames) {
+    // Add/Remove show-projects class to the specified container
+    $(".container-projects").first().toggleClass("show-projects", showGames);
+    $(".container-projects").last().toggleClass("show-projects", !showGames);
+
+    // active project tab effects
+    $("#show-games").toggleClass("btn-active", showGames);
+    $("#show-websites").toggleClass("btn-active", !showGames);
+  }
+  toggletMyWork(true);
+
+  $("#show-games").on("click", function () {
+    console.log("GAMES");
+    toggletMyWork(true);
+  });
+
+  $("#show-websites").click(function () {
+    console.log("WEBSITES");
+    toggletMyWork(false);
+  });
 });
